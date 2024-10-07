dc58c24b69ce945cc349a3d8319935ad55dc58eb
Harsh Kamal <harshonn@gmail.com>
2024-10-08 02:08:07 +0530

    Added Image

--- src/main/java/com/project/eCom/service/ProductService.java
+++ src/main/java/com/project/eCom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.project.eCom.model.Product;
 import com.project.eCom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,4 +22,13 @@ public class ProductService {
     public Product getProductById(int id) {
         return repo.findById(id).orElse(null);
     }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+
 }
